3193b9aa42a802aa650881666eaa59f7f580311d
zohebzaman12 <91787135+zohebzaman12@users.noreply.github.com>
2024-02-29 17:03:39 +0530

    Added help page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import { createBrowserRouter,RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import RootLayout from "./pages/RootLayout";
+import Faq from "./pages/Faq";
+import Contact from "./pages/Contact";
+import HelpLayout from "./pages/HelpLayout";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />}/>
       <Route path="about" element={<About />}/>
+      <Route path="help" element={<HelpLayout />}> 
+        <Route path="faq" element={<Faq />}/>
+        <Route path="contact" element={<Contact/>}/>
+      </Route>
     </Route>
   )
 )
 
 function App() {
   return (
-
       <RouterProvider  router={router}/>
-
-     
   );
 }
 
--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -8,6 +8,7 @@ const RootLayout = () => {
                 <h1>Job Portal</h1>
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="about">About</NavLink>
+                <NavLink to="help">Help</NavLink>
 
             </nav>
       </header>
