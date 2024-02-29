b8df4c6f2e83ec1fa3e7ad0ccaed97981be9a3af
zohebzaman12 <91787135+zohebzaman12@users.noreply.github.com>
2024-02-29 16:21:46 +0530

    Basic Routes setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route,createRoutesFromElements } from "react-router-dom";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import RootLayout from "./pages/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />}/>
+      <Route path="about" element={<About />}/>
+    </Route>
+  )
+)
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+
+      <RouterProvider  router={router}/>
+
+     
   );
 }
 
---
+++ src/pages/RootLayout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+const RootLayout = () => {
+  return (
+    <div className='root-layout'>
+          <header>
+            <nav>
+                <h1>Job Portal</h1>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="about">About</NavLink>
+
+            </nav>
+      </header>
+        <main>
+            <Outlet />
+        </main>
+    </div>
+  
+  )
+}
+
+export default RootLayout
